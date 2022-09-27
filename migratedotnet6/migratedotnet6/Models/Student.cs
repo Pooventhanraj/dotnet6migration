24c49adb8d84abd0199eb70cd2f3a4e3a74b8c9b
Pooventhanraj <lpooventhanraj@gmail.com>
2022-09-27 12:43:06 +0530

    first commit

---
+++ migratedotnet6/migratedotnet6/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace migratedotnet6.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string RollNo { get; set; }
+        public string Sname { get; set; }
+        public int? Std { get; set; }
+    }
+}
---
+++ migratedotnet6/migratedotnet6/Models/StudentDBContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace migratedotnet6.Models
+{
+    public partial class StudentDBContext : DbContext
+    {
+        public StudentDBContext()
+        {
+        }
+
+        public StudentDBContext(DbContextOptions<StudentDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=DESKTOP-47V9KC8;Database=StudentDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.RollNo).HasMaxLength(20);
+
+                entity.Property(e => e.Sname)
+                    .HasColumnName("SName")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Std).HasColumnName("STD");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
